736884093928f40a34b23f488d259d86d994d4e9
lpicaglia <laurent.picaglia@gmail.com>
2014-11-03 15:36:14 +0100

    test println

--- src/main/java/bataillenavale/Main.java
+++ src/main/java/bataillenavale/Main.java
@@ -4,4 +4,6 @@ package bataillenavale;
  * Created by mariealine on 03/11/14.
  */
 public class Main {
-}
+    public static void main(String[] args) {
+    System.out.println("Salut Lulu !!!");
+}}

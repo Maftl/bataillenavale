da0f6704d401a186aacecb7be1451ff3a4ecd380
Marie Aline Millot <mariealinemillot@gmail.com>
2014-11-04 08:44:34 +0100

    New Boat class and console interface

--- src/main/java/bataillenavale/Main.java
+++ src/main/java/bataillenavale/Main.java
@@ -1,12 +1,23 @@
 package bataillenavale;
+import java.util.Scanner;
 
 /**
  * Created by mariealine on 03/11/14.
  */
 public class Main {
-    public static void Main(String[] args)
+    public static void main(String[] args)
     {
-        Map map = new Map();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tu as 5 bateaux dans ton port.");
+        System.out.println("Choisis les coordonnées d'origine du bateau :");
+        System.out.println("Abscisse:");
+        String aircraftCarrierAbscissa = sc.nextLine(); 
+        System.out.println("Abscisse saisie: " + aircraftCarrierAbscissa);
+        System.out.println("Ordonnées:");
+        
+        String aircraftCarrierOrdinate = sc.nextLine();
+        System.out.println("Abscisse saisie: " + aircraftCarrierOrdinate);
+
     }
 
 }

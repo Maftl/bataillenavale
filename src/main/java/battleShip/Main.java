71e41853843e312ca11edb28e5829aa232ba6d35
Marie Aline Millot <mariealinemillot@gmail.com>
2014-11-04 10:45:37 +0100

    Input set boat working

---
+++ src/main/java/battleShip/Main.java
@@ -0,0 +1,32 @@
+package battleShip;
+import java.util.Scanner;
+
+/**
+ * Created by mariealine on 03/11/14.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        Boat myBoat = new Boat();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tu as 5 bateaux dans ton port.");
+        
+        System.out.println("Choisis l'orientation de ton premier bateau "); // à remplacer par le nom du bateau dans la boucle
+        System.out.println("ortientation : (h/v) ");
+        char direction = sc.next().charAt(0);
+        System.out.println("direction :" + direction);
+        System.out.println("Choisis les coordonnées d'origine du bateau :");
+        System.out.println("Abscisse:");
+        int abscissa = sc.nextInt();
+        System.out.println("Abscisse saisie: " + abscissa);
+        System.out.println("Ordonnées:");
+        int ordinate = sc.nextInt();
+        System.out.println("Abscisse saisie: " + ordinate);
+        
+        myBoat.initBoat(abscissa, ordinate, direction);
+
+    }
+
+}
+
+
